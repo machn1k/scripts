c533193f87145d9fd18b38cc612ea6cea122712c
Mike Machnik <mmachnik@me.com>
2013-03-23 19:36:23 -0300

    Update install-frmwrk.sh

--- install-frmwrk.sh
+++ install-frmwrk.sh
@@ -4,16 +4,22 @@
 # ---------------------------------------------------------------------------
 
 sudo mkdir /usr/local/opt/
-cd /usr/local/opt/
 
 # Metasploit
-sudo git clone https://github.com/rapid7/metasploit-framework.git /opt
+#   Need rubygems
+#wget http://production.cf.rubygems.org/rubygems/rubygems-2.0.2.zip
+#unzip rubygems-2.0.2.zip && cd rubygems-2.0.2
+#sudo ruby setup.rb
+#sudo gem install -v=1.2.3 rails
+
+cd /usr/local/opt/
+sudo git clone https://github.com/rapid7/metasploit-framework.git /msf
 sudo ln -s /usr/local/opt/msf/msf* /usr/local/bin/
 
 # Social Engineer Toolkit
 cd /usr/local/opt
-sudo git clone https://github.com/trustedsec/social-engineer-toolkit/ set/
-#MAKE WRAPPER SCRIPT IN YOUR PATH
+sudo git clone https://github.com/trustedsec/social-engineer-toolkit.git /set
+#MAKE WRAPPER SCRIPT IN PATH
 
 # w3af
 cd /usr/local/opt
@@ -25,7 +31,7 @@ sudo ln -s /usr/local/opt/w3af/w3af* /usr/local/bin/
 # Recon-ng
 cd /usr/local/opt
 sudo git clone https://bitbucket.org/LaNMaSteR53/recon-ng.git /recon-ng
-#MAKE WRAPPER SCRIPT IN YOUR PATH
+#MAKE WRAPPER SCRIPT IN PATH
 
 # Reg-Ripper
 cd /usr/local/opt && sudo mkdir rr
