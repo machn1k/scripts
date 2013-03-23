bab70b926450781f538c2639fb16d32ab188c128
Mike Machnik <mmachnik@me.com>
2013-03-23 18:58:05 -0300

    Create randMAC.sh
    Spoof a MAC address.

---
+++ randMAC.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Change MAC address in OS X or Linux
+#    Modifications to original script at 357technologies.com 
+# ---------------------------------------------------------------------------
+
+# Change MAC address in OS X or Linux
+interfaces=$1 $2
+
+prefix=(0024B2 0026F2 30469A A021B7 C03F0E C43DC7 E0469A E091F5 000FB5 00146C 00184D 001B2F 001E2A 001F33 00223F 00095B 0055D 000D88 000F3D 001195 001346 0015E9 00179A 00195B 001B11 001CF0 001E58 002191 0022B0 002401 00265A 0050BA 0080C8 14D64D 1CAFF7 1CBDB9 340804 5CD998 F07D68 000102 000103 00029C 00040B 00051A 00068C 000A04 000A5E 000BAC 000D54 000E6A 000FCB 00104B 00105A 0012A9 00147C 0016E0 00186E 001AC1 001CC5 001EC1 0020AF 002257 002473 002654 00301E 005004 005099 0050DA 006008 00608C 006097 009004 00A024 00D096 00D0D8 02608C 02C08C 08004E 20FDF1 4001C6 0002B3 000347 000423 0007E9 000CF1 000E0C 000E35 001111 0012F0 001302 001320 0013CE 0013E8 001500 001517 00166F 001676 0016EA 0016EB 0018DE 0019D1 0019D2 001B21 001B77 001CBF 001CC0 001DE0 001DE1 001E64 001E65 001E67 001F3B 001F3C 00207B 00215C 00215D 00216A 00216B 0022FA 0022FB 002314 002315 0024D6 0024D7 0026C6 0026C7 00270E 002710 0050F1 009027 00A0C9 00AA00 00AA01 00AA02 00D0B7 081196 0CD292 100BA9 183DA2 247703 4025C2 448500 4C8093 502DA2 58946B 648099 64D4DA 685D43 74E50B 78929C 809B20 88532E 8CA982 A088B4 AC7289 BC7737 DCA971 0001E6 0001E7 0002A5 0004EA 000802 000883 0008C7 000A57 000BCD 000D9D 000E7F 000EB3 000F20 000F61 001083 0010E3 00110A 001185 001279 001321 001438 0014C2 001560 001635 001708 0017A4 001871 0018FE 0019BB 001A4B 001B78 001CC4 001E0B 001F29 00215A 002264 00237D 002481 0025B3 002655 00306E 0030C1 00508B 0060B0 00805F 0080A0 080009 18A905 1CC1DE 2C27D7 3C4A92 643150 68B599 78ACC0 78E3B5 78E7D1 984BE1 B499BA B8AF67 D48564 D8D385 F4CE46 000393 000502 000A27 000A95 000D93 0010FA 001124 001451 0016CB 0017F2 0019E3 001B63 001CB3 001D4F 001E52 001EC2 001F5B 001FF3 0021E9 002241 002312 002332 00236C 0023DF 002436 002500 00254B 0025BC 002608 00264A 0026B0 0026BB 003065 0050E4 00A040 041E64 080007 1093E9 109ADD 18E7F4 24AB81 28E7CF 34159E 3C0754 40A6D9 40D32D 442A60 581FAA 5855CA 58B035 5C5948 60334B 60FB42 64B9E8 70CD60 78CA39 7C6D62 7CC537 7CF05F 88C663 8C5877 8C7B9D 9027E4 90840D 9803D8 A46706 A4B197 B8FF61 C42C03 C82A14 C8BCC8 CC08E0 D49A20 D83062 D89E3F D8A25E DC2B61 E0F847 E4CE8F E80688 F0B479 F81EDF 0000CD 0000F4 000941 000A79 000DDA 001130 001577 001AEB 002687 009099 00A0D2 ECCD6D 000E1E 001B32 0024FF 00C0DD 00E08B)
+
+length=${#prefix[*]}
+
+osx () {
+for i in ${interfaces[*]} 
+{
+    # This will generate an error... ignore it.
+    hw_prefix=${prefix[$(shuf -i 0-"$length" -n 1)]}
+    hw_suffix=$(for i in $(seq 1 6); do echo -n $(echo "obase=16; $(($RANDOM % 16))" | bc); done; echo)
+    hw_addr=$(echo "$hw_prefix$hw_suffix" | sed "s/.\{2\}/&:/g")
+    
+    echo "Changing MAC address to "${hw_addr%?}
+    sudo ifconfig $i ether ${hw_addr%?}
+    echo "Bringing down interface $i..."
+    sudo ifconfig $i down
+    echo "Bringing $i back up..."
+    sudo ifconfig $i up
+    
+    echo ""
+  }
+}
+
+linux () {
+for i in ${interfaces[*]} 
+{
+    hw_prefix=${prefix[$(shuf -i 0-"$length" -n 1)]}
+    hw_suffix=$(for i in $(seq 1 6); do echo -n $(echo "obase=16; $(($RANDOM % 16))" | bc); done; echo)
+    hw_addr=$(echo "$hw_prefix$hw_suffix" | sed "s/.\{2\}/&:/g")
+    
+    echo "Bringing down interface $i..."
+    sudo ip link set dev $i down
+    echo "Changing MAC address to "${hw_addr%?}
+    sudo ip link set dev $i address ${hw_addr%?}
+    echo "Bringing $i back up..."
+    sudo /etc/init.d/networking restart
+    #sudo ip link set dev $i up
+    
+    echo ""
+    }
+}
+
+zarg () {
+    echo -e "\tUsage: randMAC [interface 1] [interface 2]"
+    exit
+}
+
+os=$(uname -s | cut -c 1-3)
+if [ -z "$1" ] || [ $1 = "-h" ]; then
+	zarg
+else
+	if [ $os != "Dar" ]; then
+		linux
+	elif [ $os = "Dar" ]; then
+		osx
+fi;fi
