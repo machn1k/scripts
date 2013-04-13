4c2b428762b085e22fe337bfbff37739a38a0d6d
Machn1k <mmachnik@me.com>
2013-04-13 17:27:05 -0300

    Description

--- comphash.sh
+++ comphash.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-filea=$1
-fileb=$2
+fa=$1
+fb=$2
 delm="\n==================================================="
 
 # Compare Hash Values
-fm1=`openssl dgst -md5 $filea | cut -d '=' -f 2`
-fm2=`openssl dgst -md5 $fileb | cut -d '=' -f 2`
+fm1=`openssl dgst -md5 $fa | cut -d '=' -f 2`
+fm2=`openssl dgst -md5 $fb | cut -d '=' -f 2`
 
-fs1=`openssl dgst -sha1 $filea | cut -d '=' -f 2`
-fs2=`openssl dgst -sha1 $fileb | cut -d '=' -f 2`
+fs1=`openssl dgst -sha1 $fa | cut -d '=' -f 2`
+fs2=`openssl dgst -sha1 $fb | cut -d '=' -f 2`
 
 # MD5
 if [ $fm1 == $fm2 ]; then
